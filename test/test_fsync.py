98f2a25669227e4aa0b3da83f262afce67538f5f
Gergely Brautigam <skarlso777@gmail.com>
2019-01-15 06:57:40 +0100

    This is still not working.

--- test/test_fsync.py
+++ test/test_fsync.py
@@ -6,8 +6,8 @@ from fsyncer import fsyncer
 class TestFsyncer(unittest.TestCase):
     @patch('fsyncer.fsyncer.Github')
     def test_no_config_file(self, mock_hub):
-        fsyncer.main()
-        self.assertTrue(mock_hub.get_user.called, "get_user was not called")
+        fsyncer.get_repo_list()
+        mock_hub.github.Github.get_user.assert_called()
 
 
 if __name__ == '__main__':
