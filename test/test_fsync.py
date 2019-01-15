14e9c57d61c9374965ffcb639e86365d359e43dc
Gergely Brautigam <gergely.brautigam@acquia.com>
2019-01-15 09:36:33 +0100

    Environment set.

--- test/test_fsync.py
+++ test/test_fsync.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from unittest.mock import patch
 from fsyncer import fsyncer
@@ -6,6 +7,7 @@ from fsyncer import fsyncer
 class TestFsyncer(unittest.TestCase):
     @patch('fsyncer.fsyncer.Github')
     def test_no_config_file(self, mock_hub):
+        os.environ['SYNC_GITHUB_TOKEN'] = 'dummy token'
         fsyncer.get_repo_list()
         mock_hub.github.Github.get_user.assert_called()
 
