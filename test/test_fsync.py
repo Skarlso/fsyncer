29588d1e2bb232c9f302697b8f32911ec661d4aa
Gergely Brautigam <skarlso777@gmail.com>
2019-01-15 06:24:10 +0100

    Yay discovery is working.

--- test/test_fsync.py
+++ test/test_fsync.py
@@ -5,7 +5,6 @@ from fsyncer import fsyncer
 class TestFsyncer(unittest.TestCase):
     @patch('fsyncer.fsyncer.Github')
     def test_no_config_file(self, mockHub):
-        print(mockHub)
         fsyncer.main()
 
 if __name__ == '__main__':
