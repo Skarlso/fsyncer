e725a7f87883de1d5198985c3d101d95150aa0b6
Gergely Brautigam <gergely.brautigam@acquia.com>
2019-01-16 10:17:46 +0100

    WHY THE FUCK

--- test/test_fsync.py
+++ test/test_fsync.py
@@ -27,6 +27,7 @@ class TestFsyncer(unittest.TestCase):
     def test_main_with_config_file(self, mock_sync_list, _, mock_path):
         if 'TRAVIS' in os.environ:
             pass
+        print(os.environ)
         mock_path.return_value = True
         fsyncer.main()
         mock_sync_list.assert_called_with(['test_repo'])
