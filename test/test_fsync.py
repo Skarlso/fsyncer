3f352ffca5c239a49f7fe6d9ab3af0aca02ab8c8
Gergely Brautigam <gergely.brautigam@acquia.com>
2019-01-16 10:20:11 +0100

    alright.

--- test/test_fsync.py
+++ test/test_fsync.py
@@ -25,9 +25,8 @@ class TestFsyncer(unittest.TestCase):
     @patch("builtins.open", new_callable=mock_open, read_data="test_repo\n")
     @patch("fsyncer.fsyncer.sync_list")
     def test_main_with_config_file(self, mock_sync_list, _, mock_path):
-        if 'TRAVIS' in os.environ:
+        if 'TRAVIS_JOB_NAME' in os.environ:
             pass
-        print(os.environ)
         mock_path.return_value = True
         fsyncer.main()
         mock_sync_list.assert_called_with(['test_repo'])
