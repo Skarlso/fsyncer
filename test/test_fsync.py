c89ef0b898bea526840727078d45ae03109516d1
Gergely Brautigam <skarlso777@gmail.com>
2019-01-16 06:30:09 +0100

    Line break?

--- test/test_fsync.py
+++ test/test_fsync.py
@@ -1,6 +1,6 @@
 import os
 import unittest
-from unittest.mock import patch, mock_open
+from unittest.mock import patch, mock_open, MagicMock
 from fsyncer import fsyncer
 
 
@@ -8,11 +8,11 @@ class TestFsyncer(unittest.TestCase):
     @patch('fsyncer.fsyncer.Github.get_user')
     def test_get_repo_list(self, get_user):
         os.environ['SYNC_GITHUB_TOKEN'] = 'dummy token'
-        mock_repo = unittest.mock.MagicMock()
+        mock_repo = MagicMock()
         mock_repo.fork.return_value = True
         mock_repo.owner.name = 'skarlso'
 
-        mock_user = unittest.mock.MagicMock()
+        mock_user = MagicMock()
         mock_user.get_repos.return_value = [mock_repo]
         mock_user.name = 'skarlso'
 
@@ -22,10 +22,9 @@ class TestFsyncer(unittest.TestCase):
         self.assertEqual(1, len(repos), "len of %d did not equal 1" % len(repos))
 
     @patch('fsyncer.fsyncer.Path.is_file')
-    @patch("builtins.open", new_callable=mock_open, read_data="test_repo")
+    @patch("builtins.open", new_callable=mock_open, read_data="test_repo\n")
     @patch("fsyncer.fsyncer.sync_list")
-    def test_main_with_config_file(self, mock_sync_list, mock_file, mock_path):
-        mock_file = ['test_repo']
+    def test_main_with_config_file(self, mock_sync_list, _, mock_path):
         mock_path.return_value = True
         fsyncer.main()
         mock_sync_list.assert_called_with(['test_repo'])
@@ -35,6 +34,7 @@ class TestFsyncer(unittest.TestCase):
     @patch('fsyncer.fsyncer.call')
     @patch('fsyncer.fsyncer.get_repo_list')
     def test_main_without_config_file(self, mock_repo_list, mock_call, mock_path):
+        os.environ['SYNC_GITHUB_TOKEN'] = 'dummy token'
         mock_repo_list.return_value = ['no_config_test_repo']
         mock_path.return_value = False
         fsyncer.main()
