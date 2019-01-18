8be8ba4fbbd4d55d1088099b1746249890c4ff4a
Gergely Brautigam <gergely.brautigam@acquia.com>
2019-01-18 07:48:53 +0100

    Name of the readme.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import Command, find_packages, setup
 from fsyncer import __version__
 
 
-with open("README.md", "r") as fh:
+with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setup(
